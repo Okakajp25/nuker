aae8f6afe95d332f6fb57f5164909df8c6bbaff4
おかーかじぇーぴー <kaitosatou0025@gmail.com>
2023-10-18 17:12:35 +0900

    a

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import { world, system } from "@minecraft/server";
 import * as MCUI from "@minecraft/server-ui";
 
 world.afterEvents.PlayerBreakBlock.subscribe(ev => {
+    const { cancel, source } = ev;
 })
